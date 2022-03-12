900406ae51062c74545e30f2c93f85505a98a974
Aleksandra <shibaevaar@mail.ru>
2022-03-12 19:23:30 +0300

    Fixed

--- cipher.py
+++ cipher.py
@@ -6,16 +6,15 @@ while user_choice == 0:
 	print('    1 - зашифровать и расшифровать текст')
 	print('    2 - расишифровать текст')
 	user_choice = input('Выберите вариант: ')
-	if user_choice == 1:
+	if user_choice == '1':
 		text = input('Введите текст для шифрования: ')
 		shift = int(input('Введите шаг для шифрования: '))
-	elif user_choice == 2:
+	elif user_choice == '2':
 		encrypted_text = input('Введите текст: ')
 	else:
 		exit
 
-# text = 'стасичка кавров'
-# shift = 4
+
 def encrypt(text: str, shift: int) -> str:
 	result = str()
 	index = []
@@ -28,16 +27,13 @@ def encrypt(text: str, shift: int) -> str:
 			result += letter
 	return result
 
+
 def decrypt(text: str, shift: int, func=encrypt(text, shift)) -> str:
 	result = encrypt(text, shift * -1)
 	return result
 
 encrypted_text = encrypt(text, shift)
 decrypted_text = decrypt(encrypted_text, shift)
-
-# # принтим
-# print(f'"{encrypted_text}" - зашифровали')
-# print(f'"{decrypted_text}" - расшифровали')
 
 for i in range(len(alphabet)):
 	decrypted_text = decrypt(encrypted_text, i)
